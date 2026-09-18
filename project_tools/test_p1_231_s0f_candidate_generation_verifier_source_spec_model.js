5127e2d4eda19fc80e1e9dd70715eb6011f30aa6
lukindv77 <lukindv@gmail.com>
2026-09-18 13:06:42 +0700

    test(P1-231): sync S0-F exact RPF

--- project_tools/test_p1_231_s0f_candidate_generation_verifier_source_spec_model.js
+++ project_tools/test_p1_231_s0f_candidate_generation_verifier_source_spec_model.js
@@ -61,7 +61,7 @@ const CURRENT_RELATION = Object.freeze({
 });
 
 const EXPECTED_IDENTITIES = Object.freeze({
-  rpf: 'sha256:a1600deb57f2e2c8abd7698e6be00692f59a5cdfae96c61c04d0ba3436f84f1b',
+  rpf: 'sha256:b86f3e7712380331e05c1e15aafbc8fe3b35216b8a26dfb092a9a26cf57588d5',
   chromeQcf: 'sha256:3715a3453333d3d679a1c1c00a0bab6a02b77c0153f1a4e8d138aa1e8f5a984c',
   yandexQcf: 'sha256:8d6c9711b4f71b8485b49a6ab68f90bcf62bc74155ae0959dbdc4718648879a1',
   rcf: 'sha256:e6119c800c60513405541bfae552f424985e13fa109e28390ae1bac7ba075f13',
