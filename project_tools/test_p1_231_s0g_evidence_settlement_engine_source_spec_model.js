38c93913b8874b8b5f1d4e46480c68131f2b892d
lukindv77 <lukindv@gmail.com>
2026-09-18 10:17:20 +0700

    test(P1-231): sync S0-G exact RPF

--- project_tools/test_p1_231_s0g_evidence_settlement_engine_source_spec_model.js
+++ project_tools/test_p1_231_s0g_evidence_settlement_engine_source_spec_model.js
@@ -422,7 +422,7 @@ function baseReceipts(sourceSha = A) {
 
 // Canonical predecessor integration.
 test('current S0-E protocol is exact', () => assert.strictEqual(s0e.kv.protocol, 'WEBCLIP_RELEASE_IDENTITY_V1'));
-test('current S0-E RPF exact', () => assert.strictEqual(currentIds.rpf, 'sha256:5b89152a3c1d1db4b3a8a8fe4949d22ee6f82f16f47848d15ce89d94d50b811c'));
+test('current S0-E RPF exact', () => assert.strictEqual(currentIds.rpf, 'sha256:65c9a05e894c4eb1d2f5323b0497cd078c216132dc09fdad70ecebc471e8bd08'));
 test('current Chrome QCF exact', () => assert.strictEqual(currentIds.qcf['unpacked-chrome'], 'sha256:3715a3453333d3d679a1c1c00a0bab6a02b77c0153f1a4e8d138aa1e8f5a984c'));
 test('current Yandex QCF exact', () => assert.strictEqual(currentIds.qcf['yandex-e2e'], 'sha256:8d6c9711b4f71b8485b49a6ab68f90bcf62bc74155ae0959dbdc4718648879a1'));
 test('current full RCF exact', () => assert.strictEqual(currentIds.rcf, 'sha256:e6119c800c60513405541bfae552f424985e13fa109e28390ae1bac7ba075f13'));
