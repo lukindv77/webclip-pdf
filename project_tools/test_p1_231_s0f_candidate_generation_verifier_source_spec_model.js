bd3797ab322755dc3f2de8c284776bc122dced50
lukindv77 <lukindv@gmail.com>
2026-09-19 09:07:51 +0700

    test(P1-231): sync exact runtime fingerprint

--- project_tools/test_p1_231_s0f_candidate_generation_verifier_source_spec_model.js
+++ project_tools/test_p1_231_s0f_candidate_generation_verifier_source_spec_model.js
@@ -61,7 +61,7 @@ const CURRENT_RELATION = Object.freeze({
 });
 
 const EXPECTED_IDENTITIES = Object.freeze({
-  rpf: 'sha256:6b7f60999284a12040aabce4e3d9496919749a815ce4e27d3129eb06ae7249e9',
+  rpf: 'sha256:7a43fec77555a966bfe6a4ace57f785a541ae0e899decb3b43d99585874db734',
   chromeQcf: 'sha256:3715a3453333d3d679a1c1c00a0bab6a02b77c0153f1a4e8d138aa1e8f5a984c',
   yandexQcf: 'sha256:8d6c9711b4f71b8485b49a6ab68f90bcf62bc74155ae0959dbdc4718648879a1',
   rcf: 'sha256:4d4d22be60b3f2e94fc24282207889c124c2d726e4070bcab482163f58102f50',
